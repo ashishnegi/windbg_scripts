cada857778395ae3b1996de92842cacd1a578974
Ashish Negi <ashishnegi33@gmail.com>
2021-12-22 15:14:08 -0800

    Add comments and make wording better;

--- find_coro.js
+++ find_coro.js
@@ -1,3 +1,51 @@
+// Groups all coroutine frames for a coroutine function.
+// This is a best effort system. This will not give right answers as walking up the coroutine parent frames we don't know when to stop.
+// parameters:
+// coro_fn_address  : (String) Address of coroutine function for which you want to find all stack frames.
+//                    You can get this value from output of `x FabricRuntime!*TStore*TryGetValueAsync*_ResumeCoro$2`.
+// first_n          : (Int) Look at first n heaps containing coro_fn_address.
+// call_stack_depth : (Int) Depth to go down in call stack. Start from low numbers like `3`.
+// dll_base_start   : (Long) start address of functions above which only we consider coroutine walk to continue.
+// dll_base_end     : (Long) end address of functions under which we consider coroutine walk to continue.
+//                    You can get dll_base_start/end from e.g. `lmDf m FabricRuntime`
+// example usage:
+//> dx @$scriptContents.unique_coroutine_frames("00007ff62318bf80", 600, 3, 0x00007ff621310000, 0x00007ff624210000)
+// Printing Map.
+// Map size: 1
+//,0x7ff62210b9f0,0x7ff623035ee0,00007ff62318bf80 => 512
+//> u 00007ff62318bf80
+//  Fabric!Data::TStore::Store<KSharedPtr<KString>,KSharedPtr<KBuffer> >::TryGetValueAsync$_ResumeCoro$2 [C:\__w\1\s\src\prod\src\data\tstore\Store.h @ 1040]:
+//> u 0x7ff623035ee0
+//  Fabric!Data::TStore::Store<KSharedPtr<KString>,KSharedPtr<KBuffer> >::ConditionalGetAsync$_ResumeCoro$2 [C:\__w\1\s\src\prod\src\data\tstore\Store.h @ 846]:
+//> u 0x7ff62210b9f0
+//  Fabric!<lambda_82983005e362cd54ae843770683bcb01>$_ResumeCoro$2::operator() [C:\__w\1\s\src\prod\src\Store\TSReplicatedStore.cpp @ 540]:
+function unique_coroutine_frames(coro_fn_addres, first_n, call_stack_depth, dll_base_start, dll_base_end) {
+    dll_base_start = parseInt(dll_base_start, 16);
+    dll_base_end = parseInt(dll_base_end, 16);
+
+    var parents = find_all_coro_frames(coro_fn_addres, first_n);
+    var fn_stacks = new Map();
+    for (let i = 0; i < parents.length; ++i) {
+        var parent  = parents[i];
+        // log("> " + parent.co_address + " " + parent.fn_address);
+        var stack = walk_parent_chain(parent, call_stack_depth, dll_base_start, dll_base_end);
+        var fn_stack = stack.reduce(function(acc, s) { return acc + "," + s.fn_address; }, "");
+
+        if (fn_stacks.has(fn_stack)) {
+            fn_stacks.set(fn_stack, fn_stacks.get(fn_stack) + 1);
+        } else {
+            fn_stacks.set(fn_stack, 1);
+        }
+    }
+
+    log("Printing Map.\nMap size: " + fn_stacks.size);
+    for (const [key, value] of fn_stacks) {
+        log(key + " => " + value);
+    }
+
+    return fn_stacks;
+}
+
 function log(x) {
     host.diagnostics.debugLog(x + "\n")
 }
@@ -6,10 +54,22 @@ function exec(cmdstr) {
     return host.namespace.Debugger.Utility.Control.ExecuteCommand(cmdstr);
 }
 
-function find_all_coro_frames(coro_fn_addres, limit) {
-    var lines = exec("!mex.fel -x \"dq 0x${@#Line}+0x40 L1\"  !mex.head -n " + limit + " !mex.cut -f 5 !mex.grep busy !ext.heap -srch " + coro_fn_addres);
+function is_valid_address(address) {
+    return address > 0x100000; // todo: should be some high value;
+}
+
+function is_valid_function_address(address, dll_base_start, dll_base_end) {
+    return address >= dll_base_start && address <= dll_base_end;
+}
+
+function find_all_coro_frames(coro_fn_addres, first_n) {
+    var lines = exec("!mex.fel -x \"dq 0x${@#Line}+0x40 L1\"  !mex.head -n " + first_n + " !mex.cut -f 5 !mex.grep busy !ext.heap -srch " + coro_fn_addres);
     var parent_coro_frames = [];
     for (line of lines) {
+        if (!line) {
+            continue;
+        }
+
         var splits = line.split(" ");
         var parent_coro_address = splits[0].replace('`','');
         var parent_fn_address = splits[2].replace('`','');
@@ -25,16 +85,8 @@ function find_all_coro_frames(coro_fn_addres, limit) {
     return parent_coro_frames;
 }
 
-function is_valid_address(address) {
-    return address > 0x100000; // todo: should be some high value;
-}
-
-function is_valid_function_address(address, dll_base_start, dll_base_end) {
-    return address >= dll_base_start && address <= dll_base_end;
-}
-
-function walk_parent_chain(child, depth, dll_base_start, dll_base_end) {
-    if (depth <= 0) {
+function walk_parent_chain(child, call_stack_depth, dll_base_start, dll_base_end) {
+    if (call_stack_depth <= 0) {
         return [];
     }
 
@@ -67,7 +119,7 @@ function walk_parent_chain(child, depth, dll_base_start, dll_base_end) {
         }
 
         // log("  > " + parent_coro_address_str + " " + parent_fn_address);
-        var stack = walk_parent_chain({co_address: parent_coro_address, fn_address: parent_fn_address}, depth - 1, dll_base_start, dll_base_end);
+        var stack = walk_parent_chain({co_address: parent_coro_address, fn_address: parent_fn_address}, call_stack_depth - 1, dll_base_start, dll_base_end);
         stack.push(child);
         return stack;
     }
@@ -77,29 +129,4 @@ function walk_parent_chain(child, depth, dll_base_start, dll_base_end) {
     }
 
     return [child];
-}
-
-function find_coroutines(coro_fn_addres, limit, depth, dll_base_start, dll_base_end) {
-    dll_base_start = parseInt(dll_base_start, 16);
-    dll_base_end = parseInt(dll_base_end, 16);
-
-    var parents = find_all_coro_frames(coro_fn_addres, limit);
-    var fn_stacks = new Map();
-    for (let i = 0; i < parents.length; ++i) {
-        var parent  = parents[i];
-        // log("> " + parent.co_address + " " + parent.fn_address);
-        var stack = walk_parent_chain(parent, depth, dll_base_start, dll_base_end);
-        var fn_stack = stack.reduce(function(acc, s) { return acc + "," + s.fn_address; }, "");
-
-        if (fn_stacks.has(fn_stack)) {
-            fn_stacks.set(fn_stack, fn_stacks.get(fn_stack) + 1);
-        } else {
-            fn_stacks.set(fn_stack, 1);
-        }
-    }
-
-    log("size: " + fn_stacks.size);
-    for (const [key, value] of fn_stacks) {
-        log(key + " => " + value);
-    }
 }
